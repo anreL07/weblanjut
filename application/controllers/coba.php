ad9b6e7d88721f372d8db5f4920a5ab8a2227ed8
Anisyarelyda <hidayatullahanisya@gmail.com>
2022-10-25 11:20:44 +0800

    upload pertama

---
+++ application/controllers/coba.php
@@ -0,0 +1,18 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class coba extends CI_Controller {
+
+	public function rumah()
+	{
+		echo 'rumah ku';
+	}
+
+}
+
+/* End of file Controllername.php */
+
+
+?>
---
+++ application/controllers/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class web extends CI_Controller {
+
+	public function lanjut()
+	{
+		echo 'WEB Framework';
+	}
+
+}
+
+/* End of file Controllername.php */
+
+
+?>
